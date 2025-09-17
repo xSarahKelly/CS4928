6d62be9c142d0fba5be5d72831bf90a0d2efbeee
ZoeDunworth123 <22338594@studentmail.ul.ie>
2025-09-17 19:47:33 +0100

    Edit to Money and SimpleProduct

--- cafe-pos-week2/src/main/java/com/cafepos/common/SimpleProduct.java
+++ cafe-pos-week2/src/main/java/com/cafepos/common/SimpleProduct.java
@@ -1,12 +1,23 @@
-//package com.cafepos.common;
-//
-//public final class SimpleProduct implements Product {
-//    private final String id;
-//    private final String name;
-//    private final Money basePrice;
-//    public SimpleProduct(String id, String name, Money basePrice)
-//    { }
-//    @Override public String id() { return id; }
-//    @Override public String name() { return name; }
-//    @Override public Money basePrice() { return basePrice; }
-//
+package com.cafepos.common;
+
+import java.util.Objects;
+
+public final class SimpleProduct implements Product {
+    private final String id;
+    private final String name;
+    private final Money basePrice;
+    public SimpleProduct(String id, String name, Money basePrice) {
+        if (id == null || id.isBlank()){
+            throw new IllegalArgumentException("id required");
+        }
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("name required");
+        }
+        this.basePrice = Objects.requireNonNull(basePrice, "basePrice required");
+        this.id = id;
+        this.name = name;
+    }
+    @Override public String id() { return id; }
+    @Override public String name() { return name; }
+    @Override public Money basePrice() { return basePrice; }
+}
