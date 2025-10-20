dce198f21875f1bea869152ef1893b0456d42eb8
Sarah Kelly <22340793@studentmail.ul.ie>
2025-10-20 18:54:53 +0100

    test(pricing): unit tests for NoDiscount, LoyaltyPercentDiscount, FixedCouponDiscount

---
+++ cafe-pos-week2/src/test/java/com/cafepos/DiscountPolicyUnitTests.java
@@ -0,0 +1,22 @@
+package com.cafepos;
+
+import com.cafepos.common.Money;
+import com.cafepos.pricing.FixedCouponDiscount;
+import com.cafepos.pricing.LoyaltyPercentDiscount;
+import com.cafepos.pricing.NoDiscount;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiscountPolicyUnitTests {
+    @Test void no_discount_returns_zero() {
+        assertEquals(Money.zero(), new NoDiscount().discountOf(Money.of(10.00)));
+    }
+    @Test void loyalty5_is_5_percent() {
+        assertEquals(Money.of(0.50), new LoyaltyPercentDiscount(5).discountOf(Money.of(10.00)));
+    }
+    @Test void coupon1_caps_at_subtotal() {
+        var p = new FixedCouponDiscount(Money.of(1.00));
+        assertEquals(Money.of(1.00), p.discountOf(Money.of(3.30)));
+        assertEquals(Money.of(0.80), p.discountOf(Money.of(0.80)));
+    }
+}
