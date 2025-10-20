d4abe250a20cd16c60d00e951d336d35048acefb
ZoeDunworth123 <22338594@studentmail.ul.ie>
2025-10-20 22:38:07 +0100

    Edited CardPayment to ensure only digits

--- cafe-pos-week2/src/main/java/com/cafepos/payment/CardPayment.java
+++ cafe-pos-week2/src/main/java/com/cafepos/payment/CardPayment.java
@@ -1,26 +1,33 @@
 package com.cafepos.payment;
+
 import com.cafepos.order.Order;
 
 public final class CardPayment implements PaymentStrategy {
-    private final String cardNumber;
+    private final String cardNumber; // store as String
+
     public CardPayment(String cardNumber) {
-        if( cardNumber == null || cardNumber.isEmpty()){
+        if (cardNumber == null || cardNumber.isEmpty()) {
             throw new IllegalArgumentException("Card number cannot be empty");
+        }
+        // only digits allowed
+        if (!cardNumber.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("Card number must contain only digits");
         }
         this.cardNumber = cardNumber;
     }
 
     @Override
     public void pay(Order order) {
-        if(order == null){
+        if (order == null) {
             throw new IllegalArgumentException("Order cannot be null");
         }
         String maskedCard = maskCardNum(cardNumber);
         System.out.println("[Card] Customer paid " + order.totalWithTax(10) + " EUR with card " + maskedCard);
     }
+
     private String maskCardNum(String cardNumber) {
         int length = cardNumber.length();
-        if(length <=4){
+        if (length <= 4) {
             return cardNumber;
         }
         return "****" + cardNumber.substring(length - 4);
