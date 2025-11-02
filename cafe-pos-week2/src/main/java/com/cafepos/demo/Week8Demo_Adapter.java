d504db257b143b1a46ebbd94780d4683a027c3d4
ZoeDunworth123 <22338594@studentmail.ul.ie>
2025-11-02 22:11:46 +0000

    Adapter pattern added

---
+++ cafe-pos-week2/src/main/java/com/cafepos/demo/Week8Demo_Adapter.java
@@ -0,0 +1,12 @@
+package com.cafepos.demo;
+import com.cafepos.checkout.ReceiptPrinter;
+import com.cafepos.printing.*;
+import vendor.legacy.LegacyThermalPrinter;
+public final class Week8Demo_Adapter {
+public static void main(String[] args) {
+String receipt = "Order (LAT+L) x2\nSubtotal: 7.80\nTax (10%): 0.78\nTotal: 8.58";
+Printer printer = new LegacyPrinterAdapter(new LegacyThermalPrinter());
+printer.print(receipt);
+System.out.println("[Demo] Sent receipt via adapter.");
+}
+}
---
+++ cafe-pos-week2/src/main/java/com/cafepos/printing/Printer.java
@@ -0,0 +1,5 @@
+package com.cafepos.printing;
+
+public interface Printer {
+    void print(String receiptText);
+}
